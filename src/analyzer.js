2cfcbee34b06fbd5ef7de89c8e940c7fdca0bc02
leslie pacheco <lesli_sharai_pacheco@hotmail.com>
2023-08-29 18:16:55 -0600

    se agrego la funcionalidad del conteo de palabras, caracteres etc..

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,66 @@
 const analyzer = {  
   getWordCount: (text) => {
+    let words = text.trim();
+
+    let worsExcluiSpaces = words.split(' ');
+
+    return worsExcluiSpaces.length;
+
+
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+
+    let  counCharter =  text.replace(/[\s.,;]/g, "");
+
+    return counCharter.length;
+
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => { 
+    
+    let words = text.trim();
+
+    let worsExcluiSpaces = words.split(' ').length; // total de palabras
+
+    let charters =  text.length
+
+    let longitud = charters / worsExcluiSpaces;
+
+    return longitud;
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+
+    let  getNumber =  text.match(/(\d+)/g).length;
+
+    if(getNumber >0){
+
+      return getNumber;
+
+    }else{
+      return 0;
+    }
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+
+      let getSum = 0;
+
+      let  getNumber =  text.match(/(\d+)/g);
+
+      for (let i = 0; i < getNumber.length; i++){
+
+            getSum = getSum + parseInt(getNumber[i]);
+
+      }
+
+      return getSum;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import analyzer from './analyzer.js';
 
+    let text = document.querySelector("textarea[name='user-input']");
+    
+
+    text.addEventListener('keyup',function (){
+
+        document.querySelector(".word-count").innerHTML = 'Palabras: '+analyzer.getWordCount(text.value);
+    
+        document.querySelector(".character-count").innerHTML = 'Caracteres: '+analyzer.getCharacterCount(text.value);
+
+        document.querySelector(".character-no-spaces-count").innerHTML = 'Caracteres sin espacios: '+ analyzer.getCharacterCountExcludingSpaces(text.value);
+
+        document.querySelector(".word-length-average").innerHTML = 'Promedio de longitud: '+analyzer.getAverageWordLength(text.value);
+
+        document.querySelector(".number-count").innerHTML = 'Numeros: '+analyzer.getNumberCount(text.value);
+
+        document.querySelector(".number-sum").innerHTML = 'Suma de numeros: '+analyzer.getNumberSum(text.value);
+
+    });
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
