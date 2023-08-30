6e43bf7b473bad914c9d72583fe47827d5ecc428
leslie pacheco <lesli_sharai_pacheco@hotmail.com>
2023-08-29 18:21:17 -0600

    se agregó la función del botón de reseteo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import analyzer from './analyzer.js';
 
     let text = document.querySelector("textarea[name='user-input']");
+    let ResetBotton = document.getElementById("reset-button");
     
 
     text.addEventListener('keyup',function (){
@@ -16,6 +17,24 @@ import analyzer from './analyzer.js';
         document.querySelector(".number-count").innerHTML = 'Numeros: '+analyzer.getNumberCount(text.value);
 
         document.querySelector(".number-sum").innerHTML = 'Suma de numeros: '+analyzer.getNumberSum(text.value);
+
+    });
+
+    ResetBotton.addEventListener('click',() =>{
+
+        document.querySelector("textarea[name='user-input']").value = '';
+
+        document.querySelector(".character-count").innerHTML = 'Caracteres: 0';
+
+        document.querySelector(".character-no-spaces-count").innerHTML = 'Caracteres sin espacios: 0';
+
+        document.querySelector(".word-count").innerHTML = 'Palabras: 0';
+
+        document.querySelector(".word-length-average").innerHTML = 'Promedio de longitud: 0';
+
+        document.querySelector(".number-count").innerHTML = 'Numeros: 0';
+
+        document.querySelector(".number-sum").innerHTML = 'Suma de numeros: 0';
 
     });
 
